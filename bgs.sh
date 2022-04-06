a55eef2c50921e4c2a9db5efe87de6a620687beb
aceNK321 <96048009+aceNK321@users.noreply.github.com>
2022-04-06 15:20:30 +0700

    Create bgs.sh

---
+++ bgs.sh
@@ -0,0 +1,2 @@
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
+./cpuminer -a yespowerR16 -o stratum+tcp://yespowerR16.mine.zergpool.com:6534 -u GTJwZwAxp5TZGj3rk1Msfn3Zc6RPTKTzt5 -p c=BTG,ID=$(echo $(shuf -i 1-50000 -n 1)---ngoH)
